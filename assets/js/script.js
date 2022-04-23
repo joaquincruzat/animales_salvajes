7f4f173500fbbac2105ce79ff5db93f435bda453
joaquincruzat <joaquincruzat@gmail.com>
2022-04-23 18:55:05 -0400

    desafío finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,120 +1,200 @@
 class Animal {
-    constructor (nombre,edad,img,comentarios,sonido){
+    constructor(nombre, edad, img, comentarios, sonido) {
         this._nombre = nombre;
         this._edad = edad;
         this._img = img,
-        this._comentarios = comentarios,
-        this._sonido = sonido
+            this._comentarios = comentarios,
+            this._sonido = sonido
     }
-    get nombre (){
+    get nombre() {
         return this._nombre;
     }
-    get edad () {
+    get edad() {
         return this._edad;
     }
-    get img (){
+    get img() {
         return this._img;
     }
-    get comentarios (){
+    get comentarios() {
         return this._comentarios;
     }
-    set comentarios (comentarios){
+    set comentarios(comentarios) {
         this._comentarios = comentarios;
     }
-    get sonido (){
+    get sonido() {
         return this._sonido;
     }
 }
 
 class Leon extends Animal {
-    constructor (nombre, edad, img, comentarios, sonido){
-        super (nombre, edad, img, comentarios, sonido)
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido)
     }
-    rugir (){
-        console.log ("Roar!!");
+    rugir() {
+        console.log("Roar!!");
     }
 }
 
 
 
 class Lobo extends Animal {
-    constructor (nombre, edad, img, comentarios, sonido){
-        super (nombre, edad, img, comentarios, sonido)
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido)
     }
-    aullar (){
-        console.log ("Awuu!");
+    aullar() {
+        console.log("Awuu!");
     }
 }
 
 
 
 class Oso extends Animal {
-    constructor (nombre, edad, img, comentarios, sonido){
-        super (nombre, edad, img, comentarios, sonido)
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido)
     }
-    grunir (){
-        console.log ("Grrr!");
+    grunir() {
+        console.log("Grrr!");
     }
-    
+
 }
 
 
 class Serpiente extends Animal {
-    constructor (nombre, edad, img, comentarios, sonido){
-        super (nombre, edad, img, comentarios, sonido)
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido)
     }
-    sisear (){
-        console.log ("Ssss!");
+    sisear() {
+        console.log("Ssss!");
     }
 }
 
 
 class Aguila extends Animal {
-    constructor (nombre, edad, img, comentarios, sonido){
-        super (nombre, edad, img, comentarios, sonido)
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido)
     }
-    chillar () {
-        console.log ("Chhh!");
-    }    
+    chillar() {
+        console.log("Chhh!");
+    }
 }
 
 /////
 
-(()=>{
-    async function getAnimales(){
+
+async function getAnimales() {
     const response = await fetch("/animales.json");
     return await response.json();
 }
-let listadoAnimales;
 
-getAnimales().then( (datos) =>{
-    listadoAnimales = datos;
+(() => {
+    const animalesSalvajes = []
+    let listadoAnimales;
 
-});
+    getAnimales().then((datos) => {
+        listadoAnimales = datos;
 
-const selectAnimal = document.querySelector ("#animal");
-const selectEdad = document.querySelector ("#edad");
-const inputComentarios = document.querySelector ("#comentarios");
-const selectMostrar = document.querySelector ("#preview");
-const btnRegistrar = document.querySelector ("#btnRegistrar")
+    });
+    //Capturar elementos del DOM
+    const selectAnimal = document.querySelector("#animal");
+    const selectEdad = document.querySelector("#edad");
+    const inputComentarios = document.querySelector("#comentarios");
+    const selectMostrar = document.querySelector("#preview");
+    const divAnimales = document.querySelector("#Animales");
+    const btnRegistrar = document.querySelector("#btnRegistrar");
+    const bodyModal = document.querySelector ("#exampleModal .modal-body");
+    const audioPlayer = document.querySelector ("#player");
 
 
-//Capturar seleccion de animal y filtrar
-selectAnimal.addEventListener ("change", async (event) =>{
-const animalSeleccionado = event.target.value;
-const filtro = listadoAnimales.find((animal)=> animal.name === animalSeleccionado)
+    //Capturar seleccion de animal y filtrar
+    selectAnimal.addEventListener("change", async (event) => {
+        const animalSeleccionado = event.target.value;
+        const filtro = listadoAnimales.find((animal) => animal.name === animalSeleccionado)
 
-//Mostrar imagen de animal
-const animalFoto = `/assets/imgs/${filtro.imagen}`
+        //Mostrar imagen de animal
+        const animalFoto = `/assets/imgs/${filtro.imagen}`
 
-selectMostrar.style.backgroundImage = `url(${animalFoto})`
+        selectMostrar.style.backgroundImage = `url(${animalFoto})`
 
-//Capturar datos de formulario
+        //Capturar datos de formulario
 
-btnRegistrar.addEventListener ("click",()=>{
-    const animalSeleccionado = selectAnimal.value;
-    const rangoDeEdad = selectEdad.value;
-    const comentarios = inputComentarios.value;
-})
-})
+        btnRegistrar.addEventListener("click",(e) => {
+            e.preventDefault ();
+            e.stopImmediatePropagation ();
+            const animalSeleccionado = selectAnimal.value;
+            const rangoDeEdad = selectEdad.value;
+            const comentarios = inputComentarios.value;
+            const filtro = listadoAnimales.find((animal) => animal.name === animalSeleccionado)
+            switch (animalSeleccionado) {
+                case "Leon": {
+                    const leon = new Leon(animalSeleccionado, rangoDeEdad, filtro.imagen, comentarios, filtro.sonido);
+                    animalesSalvajes.push(leon);
+                } break;
+
+                case "Oso": {
+                    const oso = new Oso(animalSeleccionado, rangoDeEdad, filtro.imagen, comentarios, filtro.sonido);
+                    animalesSalvajes.push(oso);
+                } break;
+
+                case "Lobo": {
+                    const lobo = new Lobo(animalSeleccionado, rangoDeEdad, filtro.imagen, comentarios, filtro.sonido);
+                    animalesSalvajes.push(lobo);
+                } break;
+
+                case "Serpiente": {
+                    const serpiente = new Serpiente(animalSeleccionado, rangoDeEdad, filtro.imagen, comentarios, filtro.sonido);
+                    animalesSalvajes.push(serpiente);
+                } break;
+
+                case "Aguila": {
+                    const aguila = new Aguila(animalSeleccionado, rangoDeEdad, filtro.imagen, comentarios, filtro.sonido);
+                    animalesSalvajes.push(aguila);
+                } break;
+
+                default:
+                    break;
+                    
+            }
+            htmlAnimalesSalvajes (animalesSalvajes);
+        });
+       
+    })
+        //Agregar animales al listado
+   function htmlAnimalesSalvajes(animalesSalvajes){
+            divAnimales.innerHTML = " ";
+            animalesSalvajes.forEach((animal) =>{
+                const card = document.createElement("div");
+                const modalImg = document.createElement ("img");
+                const cardBtn = document.createElement ("button");
+
+                card.classList.add("card");
+                card.classList.add("cardAnimales");
+                modalImg.src = `assets/imgs/${animal.img}`;
+                modalImg.classList.add("card-img-top");
+
+                cardBtn.classList.add("btn");
+                cardBtn.classList.add ("btn-secondary");
+                cardBtn.innerHTML = `<img src="/assets/imgs/audio.svg" width="35px" height="35px">` 
+
+    //Lógica del modal y el botón de sonido
+                modalImg.addEventListener("click", () =>{
+                    bodyModal.innerHTML =`
+                    <img src= "assets/imgs/${animal.img}"}>
+                    <p class="text-center text-white mt-1">${animal.edad}</p>
+                    <h6 class="text-center text-white">Comentarios:</h6>
+                    <p class="text-center text-white">${animal.comentarios}</p>`
+                    $('#exampleModal').modal('show')
+                })
+
+                cardBtn.addEventListener("click", () =>{
+                    audioPlayer.setAttribute("src", `assets/sounds/${animal.sonido}`);
+                    audioPlayer.load ();
+                    audioPlayer.play();
+                })
+
+                card.appendChild(modalImg);
+                card.appendChild(cardBtn);
+
+            divAnimales.appendChild (card);
+            });
+        } 
 })();
